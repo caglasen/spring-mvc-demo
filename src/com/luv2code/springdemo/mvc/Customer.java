c126b81d5c4eb2be6eab898cd162c11e554ba348
Cagla Sen <cagla1876@hotmail.com>
2021-10-01 14:52:38 +0300

    "Lecture 157- Applying Regular Expressions -Write Some Code" is finished

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -1,16 +1,32 @@
 package com.luv2code.springdemo.mvc;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Customer {
 	private String firstName;
 	
-	@NotNull(message="is required!")
-	@Size(min=1)
+	@NotNull(message = "is required!!!")
+	@Size(min=1, message="is required!")
 	private String lastName;
 	
 	
+	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 characters or digits")
+	private String postalCode;
+	
+	@Min(value=0, message="must be greater than zero")
+	@Max(value=10, message="must be less than or equal to 10")
+	private int freePasses;
+	
+	public int getFreePasses() {
+		return freePasses;
+	}
+	public void setFreePasses(int freePasses) {
+		this.freePasses = freePasses;
+	}
 	public String getFirstName() {
 		return firstName;
 	}
@@ -22,6 +38,12 @@ public class Customer {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
 	}
 
 	
